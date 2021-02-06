b078632b73c30dd088e96b44fd0e7b48624384f9
Jorge <jorge.caro99@gmail.com>
2021-02-06 11:55:05 +0100

    Created header

---
+++ app/public/wp-content/themes/ignaweb/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?> <!--carga hoja de estilos -->
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header class="site-header">
+        <div class="navBar">
+            <div class="logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/ignaser.png" width="100" alt="logo pagina">
+            </div>
+            <?php
+            $args = array(
+                'theme_location' => 'menu-principal',
+                'container' => 'nav',
+                'container_class' => 'menuPrincipal'
+            );
+            wp_nav_menu($args);
+            ?>
+        </div>
+    </header>
--- app/public/wp-content/themes/ignaweb/index.php
+++ app/public/wp-content/themes/ignaweb/index.php
@@ -1,24 +1,3 @@
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IgnaWeb</title>
-    <?php wp_head(); ?>
-</head>
-
-<body>
-    <?php
-    $args = array(
-        'theme_location' => 'menu-principal',
-        'container' => 'nav',
-        'container_class' => 'menu-principal'
-    );
-    //wp_nav_menu($args);
-    ?>
-    <h1>Ignaweb theme</h1>
-</body>
-
-</html>
+<?php get_header(); ?>
+<h1>Inicio</h1>
