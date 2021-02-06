30c4643a15a98dfb9bab3c7f028d211eb4032303
Alfredo <manjon.canela@gmail.com>
2021-02-06 10:43:55 +0100

    add functions.php

--- app/public/wp-content/themes/ignaweb/index.php
+++ app/public/wp-content/themes/ignaweb/index.php
@@ -1,1 +1,24 @@
-<h1>Ignaweb theme</h1>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IgnaWeb</title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <?php
+    $args = array(
+        'theme_location' => 'menu-principal',
+        'container' => 'nav',
+        'container_class' => 'menu-principal'
+    );
+    //wp_nav_menu($args);
+    ?>
+    <h1>Ignaweb theme</h1>
+</body>
+
+</html>
