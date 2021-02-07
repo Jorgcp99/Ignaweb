284d4a2907d351af8f70520fc857b14633e826e5
Jorge <jorge.caro99@gmail.com>
2021-02-07 14:03:27 +0100

    Added lightbox to gallery screen

--- app/public/wp-content/themes/ignaweb/functions.php
+++ app/public/wp-content/themes/ignaweb/functions.php
@@ -17,9 +17,10 @@ function pickApp_scripts_styles()
 
     wp_enqueue_style('styles', get_stylesheet_uri(), array('googleFont', 'normalize'), '1.0.0');
 
-    wp_enqueue_style('slickNavCSS', get_template_directory_uri() . '/css/slicknav.min.css', array(), '1.0.0');
-
-    wp_enqueue_script('slickNavJS', get_template_directory_uri() . '/js/jquery.slicknav.min.js', array('jquery'), '1.0.0', true);
+    if (is_page('galeria')) :
+        wp_enqueue_style('lightboxCSS', get_template_directory_uri() . '/css/lightbox.min.css', array(), '2.11.3');
+        wp_enqueue_script('lightboxJS', get_template_directory_uri() . '/js/lightbox.min.js', array(), '2.11.3', true);
+    endif;
 
     wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery', 'slickNavJS'), '1.0.0', true);
 }
