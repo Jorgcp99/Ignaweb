c414f67fb18b6cbb5e03035174f73da7913560fd
Jorge <jorge.caro99@gmail.com>
2021-02-07 13:32:49 +0100

    Added footer

--- app/public/wp-content/themes/ignaweb/footer.php
+++ app/public/wp-content/themes/ignaweb/footer.php
@@ -1,5 +1,18 @@
 <footer class="siteFooter">
-    <p>Site footer</p>
+    <div class="footerContent">
+        <div class="footerDireccion">
+            <h4 class="footerTit">Dirección</h4>
+            <p class="footerInfo">C/Holanda Nº24</p>
+            <p class="footerInfo">28341 Valdemoro, Madrid</p>
+        </div>
+        <h3 class="copyright"><?php echo get_bloginfo('name') . " ® " . date('Y') ?></h3>
+        <div class="footerContacto">
+            <h4 class="footerTit">Contacto</h4>
+            <p class="footerInfo">Telf: 676938423</p>
+            <p class="footerInfo">info@ignaser.com</p>
+        </div>
+    </div>
+
 </footer>
 
 <?php wp_footer(); ?>
